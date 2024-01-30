5f2291f6cab9f6033806b8043c4785518ddec5d0
Uemerson Pinheiro Junior <uemersonpinheirojunior@gmail.com>
2024-01-29 23:40:43 -0300

    feat: add category and product database

---
+++ goapi/internal/database/product_db.go
@@ -0,0 +1,69 @@
+package database
+
+import (
+	"database/sql"
+
+	"github.com/Uemerson/fullcycle-immersion-17/goapi/internal/entity"
+)
+
+type ProductDB struct {
+	db *sql.DB
+}
+
+func NewProductDB(db *sql.DB) *ProductDB {
+	return &ProductDB{db: db}
+}
+
+func (cd *ProductDB) GetProducts() ([]*entity.Product, error) {
+	rows, err := cd.db.Query("SELECT id, name, description, price, category_id, image_url FROM products")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var products []*entity.Product
+	for rows.Next() {
+		var Product entity.Product
+		if err := rows.Scan(&Product.ID, &Product.Name, &Product.Description, &Product.Price, &Product.CategoryID, &Product.ImageURL); err != nil {
+			return nil, err
+		}
+		products = append(products, &Product)
+	}
+	return products, nil
+}
+
+func (cd *ProductDB) GetProduct(id string) (*entity.Product, error) {
+	var Product *entity.Product
+	err := cd.db.QueryRow("SELECT id, name, description, price, category_id, image_url FROM products WHERE id = ?", id).Scan(&Product.ID, &Product.Name, &Product.Description, &Product.Price, &Product.CategoryID, &Product.ImageURL)
+	if err != nil {
+		return nil, err
+	}
+	return Product, nil
+}
+
+func (cd *ProductDB) GetProductByCategoryID(categoryID string) ([]*entity.Product, error) {
+	rows, err := cd.db.Query("SELECT id, name, description, price, category_id, image_url FROM products WHERE category_id = ?", categoryID)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var products []*entity.Product
+	for rows.Next() {
+		var Product entity.Product
+		if err := rows.Scan(&Product.ID, &Product.Name, &Product.Description, &Product.Price, &Product.CategoryID, &Product.ImageURL); err != nil {
+			return nil, err
+		}
+		products = append(products, &Product)
+	}
+	return products, nil
+}
+
+func (cd *ProductDB) CreateProduct(product *entity.Product) (*entity.Product, error) {
+	_, err := cd.db.Exec("INSERT INTO products (id, name, description, price, category_id, image_url) VALUES (?, ?, ?, ?, ?, ?)",
+		product.ID, product.Name, product.Description, product.Price, product.CategoryID, product.ImageURL)
+	if err != nil {
+		return nil, err
+	}
+	return product, nil
+}
